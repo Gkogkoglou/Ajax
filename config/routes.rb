6328cc1c5d14f5fa650fc4a86f8785f04e132cfd
Konstantinos <konstantinos.gkogkoglou@gmail.com>
2016-10-20 10:15:23 +0200

    Finished up the backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :todos
+  # show our todo index page for as root page
+  root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
